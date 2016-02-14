999dc06dab7ff92445c2787d26884ccdf57cddac
Sofia <sofia.big@live.com>
2016-02-13 23:13:11 -0200

    Diretorios arrumados

--- python_intro.py
+++
@@ -1,9 +0,0 @@
-#def hi(name):
-#	print('Hi '+name+'!')
-#
-#girls = ['Rachel','Monica','Phoebe','Sofia','Anonimo']
-#for name in girls:
-#	hi(name)
-#	print('Next Girl')
-for i in range(1,6):
-	print(i)
